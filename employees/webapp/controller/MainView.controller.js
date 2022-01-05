5f0ef7b76ad694804303fe85d12986157587b148
briyajaira <briyajaira@gmail.com>
2022-01-05 18:41:55 +0000

    Modelos

--- employees/webapp/controller/MainView.controller.js
+++ employees/webapp/controller/MainView.controller.js
@@ -1,3 +1,5 @@
+import { stringify } from "querystring";
+
 sap.ui.define([
     "sap/ui/core/mvc/Controller"
 ],
@@ -7,21 +9,57 @@ sap.ui.define([
     function (Controller) {
         "use strict";
 
+        function onInit() {
+
+            var oJSONModel = new sap.ui.model.json.JSONModel();
+            var oView = this.getView();
+            var i18nBundle = oView.getModel("i18n").getResourceBundle();
+
+            // var oJSON = {
+            //     employeeId: "12345",
+            //     countryKey: "UK",
+            //     listCountry: [
+            //         {
+            //             key: "US",
+            //             text: i18nBundle.getText("countryUS")
+            //         },
+            //         {
+            //             key: "UK",
+            //             text: i18nBundle.getText("countryUK")
+            //         },
+            //         {
+            //             key: "ES",
+            //             text: i18nBundle.getText("countryES")
+            //         }
+            //     ]
+            // };
+
+        //    oJSONModel.setData(oJSON);
+             oJSONModel.loadData("../localService/mockdata/Employees.json", false);
+             oJSONModel.attachRequestCompleted(function(oEventModel){
+                 console.log(JSON.stringify(oJSONModel.getData()));
+             });
+            oView.setModel(oJSONModel);
+        }
+
         var Main = Controller.extend("logaligroup.employees.controller.MainView", {});
 
         Main.prototype.onValidate = function () {
-                var inputEmployee = this.byId("inputEmployee");
-                var valueEmployee = inputEmployee.getValue();
+            var inputEmployee = this.byId("inputEmployee");
+            var valueEmployee = inputEmployee.getValue();
 
-                if (valueEmployee.length === 6) {
-                   // inputEmployee.setDescription("OK");
-                   this.getView().byId("labelCountry").setVisible(true);
-                   this.getView().byId("slCountry").setVisible(true);
-                } else {
-                   // inputEmployee.setDescription("Not OK");
-                   this.getView().byId("labelCountry").setVisible(false);
-                   this.getView().byId("slCountry").setVisible(false);
-                }
-            };
-            return Main;
-        });
+            if (valueEmployee.length === 6) {
+                // inputEmployee.setDescription("OK");
+                this.getView().byId("labelCountry").setVisible(true);
+                this.getView().byId("slCountry").setVisible(true);
+            } else {
+                // inputEmployee.setDescription("Not OK");
+                this.getView().byId("labelCountry").setVisible(false);
+                this.getView().byId("slCountry").setVisible(false);
+            }
+        };
+
+        Main.prototype.onInit = onInit;
+
+        return Main;
+    });
