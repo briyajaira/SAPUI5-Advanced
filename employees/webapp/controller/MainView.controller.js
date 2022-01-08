9a88657fcb45ec0fff5b3246df2041077a893978
briyajaira <briyajaira@gmail.com>
2022-01-08 15:28:08 +0000

    Contexto y Mensaje

--- employees/webapp/controller/MainView.controller.js
+++ employees/webapp/controller/MainView.controller.js
@@ -6,6 +6,8 @@ sap.ui.define([
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
+     * @param {typeof sap.ui.model.Filter} Filter
+     * @param {typeof sap.ui.model.FilterOperator} FilterOperator
      */
     function (Controller, Filter, FilterOperator) {
         "use strict";
@@ -66,6 +68,15 @@ sap.ui.define([
             var oModel = this.getView().getModel();
             oModel.setProperty("/EmployeeId", "");
             oModel.setProperty("/CountryKey", "");
+        };
+
+        function showPostalCode(oEvent) {
+            var itemPressed = oEvent.getSource();
+            var oContext = itemPressed.getBindingContext();
+            var objectContext = oContext.getObject();
+
+            sap.m.MessageToast.show(objectContext.PostalCode);
+        
         }
 
             var Main = Controller.extend("logaligroup.employees.controller.MainView", {});
@@ -88,6 +99,7 @@ sap.ui.define([
             Main.prototype.onInit   = onInit;
             Main.prototype.onFilter = onFilter;
             Main.prototype.onClearFilter = onClearFilter;
+            Main.prototype.showPostalCode = showPostalCode;
     
             return Main;
 
